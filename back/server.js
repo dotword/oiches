955186ab9f58a62e9fac3b2129bccab2953c29b0
Carmen Salgueiro <carmen@dotword.es>
2024-06-21 15:17:28 +0200

    nodemon

--- back/server.js
+++ back/server.js
@@ -1,18 +1,22 @@
-"use strict";
+'use strict';
 
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
 
-import express from "express";
+import express from 'express';
 
 const app = express();
 
 const { PORT } = process.env;
 
-app.get("/", (req, res) => {
-  res.send("Hola desde Oiches!!");
+app.get('/', (req, res) => {
+    res.send('Hola desde Oiches!!');
+});
+
+app.use((req, res) => {
+    res.status(404).send('Recurso no encontrado');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server on ${PORT}`);
+    console.log(`Server on ${PORT}`);
 });
