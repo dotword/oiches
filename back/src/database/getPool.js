3a10b57c6d5f802da38ef3e03685d02843b1a67d
Carmen Salgueiro <carmen@dotword.es>
2024-06-27 12:45:19 +0200

    Creado getPool

--- back/src/database/getPool.js
+++ back/src/database/getPool.js
@@ -1,1 +1,27 @@
-//AQUI VA getPool PARA CONEXION BBDD
+import dotenv from 'dotenv';
+import mysql from 'mysql2/promise';
+
+dotenv.config();
+const { MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE } = process.env;
+
+let pool;
+const getPool = async () => {
+    try {
+        if (!pool) {
+            pool = mysql.createPool({
+                connectionLimit: 10,
+                host: MYSQL_HOST,
+                user: MYSQL_USER,
+                password: MYSQL_PASSWORD,
+                database: MYSQL_DATABASE,
+                timezone: 'Z',
+            });
+        }
+
+        return await pool;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export default getPool;
