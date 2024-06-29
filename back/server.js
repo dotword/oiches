61c821fa415d192da4a016e231afcab1a40afb8d
Aitor Carreras <pqpiacarreras@gmail.com>
2024-06-29 21:11:32 +0800

    server

--- back/server.js
+++ back/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
 import routes from './src/routes/index.js';
+import getPool from './src/database/getPool.js';
 
 const { PORT } = process.env;
 
@@ -11,6 +12,18 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 /**Llamado a rutas */
+app.post('/reservar-sala', async(req,res,next)=>{
+  try {
+    const pool =  await getPool()
+    console.log(req.headers,req.body);
+    res.sendStatus(200).json({
+      message:req
+    })
+  } catch (error) {
+    console.log(error);
+    next(error)
+  }
+})
 app.use(routes);
 
 app.use((req, res) => {
