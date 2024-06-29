e919fe346ddfa032790c4e0546eed1680ee5d961
Aitor Carreras <pqpiacarreras@gmail.com>
2024-06-29 21:17:54 +0800

    Rutas Controladres Fix

--- back/src/routes/usersRouter.js
+++ back/src/routes/usersRouter.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import { registerUserController } from '../controllers/users/index.js';
 import validateUserController from '../controllers/users/validateUserController.js'
+import { loginUserController } from '../controllers/users/loginUserController.js';
+import { passwordChangeController } from '../controllers/users/passwordChangeController.js';
 
 const router = express.Router();
 
 // Endpoint registro de usuarios
 router.post('/users/registro', registerUserController);
 
-router.post('/users/login', registerUserController);
-router.patch('/users/password', registerUserController);
+router.post('/users/login', loginUserController);
+router.patch('/users/password', passwordChangeController);
 
 //Endpoint validación de usuarios
 router.put('/users/validate/:registrationCode',validateUserController);
