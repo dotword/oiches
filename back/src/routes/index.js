c8552c121e42adbfea210bc25cb8aece1681f0f6
Aitor Carreras <pqpiacarreras@gmail.com>
2024-06-30 20:11:41 +0800

    Reservas Router

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -3,11 +3,12 @@ import express from 'express';
 import usersRouter from './usersRouter.js';
 import salasRouter from './salasRouter.js';
 import musicosRouter from './musicosRouter.js';
-
+import reservasRouter from './reservasRoutes.js'
 const router = express.Router();
 
 router.use(usersRouter);
 router.use(salasRouter);
 router.use(musicosRouter);
+router.use(reservasRouter);
 
 export default router;
