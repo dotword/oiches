3fe5a09a55b13abc8fd215582df8459960c75e6f
Aitor Carreras <155743022+aitorz09@users.noreply.github.com>
2024-07-01 00:27:58 +0800

    Manejo de errores crear reserva.js

--- back/src/controllers/reservas/crearReservaController.js
+++ back/src/controllers/reservas/crearReservaController.js
@@ -3,14 +3,19 @@ export const crearReservaController = async (req,res,next) =>{
   try {
     const { fecha, hora, nombre } = req.body;
     if(!fecha || !hora || !nombre){
-     return res.status(404).json({
+     return res.status(400).json({
         message:'Es necesario la fecha, hora y el nombre de la reserva.'
       })
     }
     const { token } = req.headers;
+    if (!token) {
+    return res.status(401).json({
+    message: 'Es necesario proporcionar un token de autorización.'
+    });
+    }
     const { sala_id } = req.params;
     if(!sala_id){
-      return res.status(404).json({
+      return res.status(400).json({
         message:"Es necesario seleccionar una sala para reservar."
       })
                                   
