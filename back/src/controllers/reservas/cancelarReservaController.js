b79648b9ba6c3fabe9bd63aef8121fd80a7be46c
Aitor Carreras <pqpiacarreras@gmail.com>
2024-06-30 19:36:15 +0800

    Cancelar reserva EndPoint

---
+++ back/src/controllers/reservas/cancelarReservaController.js
@@ -0,0 +1,15 @@
+import { cancelarReservaService } from "../../services/reservas/cancelarReservaService.js";
+export const cancelarReservaController= async (req,res,next) =>{
+try {
+  const { token } = req.headers;
+  const sala_id = req.params
+  
+  await cancelarReservaService(token,sala_id)
+ 
+  res.status(200).json({
+    message:"Se ha borrado la reserva con exito ",
+  })
+} catch (error) {
+  next(error)
+}
+}
