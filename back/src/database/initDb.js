fe608cf00a765a338a115cd46c4dc3c90e5ff0ec
Carmen Salgueiro <carmen@dotword.es>
2024-06-30 20:24:03 +0200

    detalle sala comentarios

--- back/src/database/initDb.js
+++ back/src/database/initDb.js
@@ -55,24 +55,24 @@ const main = async () => {
 
         // Creando tabla Grupos
         await pool.query(`
-    CREATE TABLE IF NOT EXISTS Grupos(
-id INT AUTO_INCREMENT PRIMARY KEY,
-nombre VARCHAR(50)  NOT NULL UNIQUE,
-provincia VARCHAR(255),
-honorarios INT,
-biografia TEXT,
-genero VARCHAR(255),
-usuario_id INT,
-FOREIGN KEY (usuario_id) REFERENCES Usuarios(id),
-rider VARCHAR(255),
-avatar VARCHAR(255),
-enlaces VARCHAR(255),
-email VARCHAR(255),
-createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
-updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-deletedAt DATETIME NULL
-);
-    `);
+            CREATE TABLE IF NOT EXISTS Grupos(
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                nombre VARCHAR(50)  NOT NULL UNIQUE,
+                provincia VARCHAR(255),
+                honorarios INT,
+                biografia TEXT,
+                genero VARCHAR(255),
+                usuario_id INT,
+                FOREIGN KEY (usuario_id) REFERENCES Usuarios(id),
+                rider VARCHAR(255),
+                avatar VARCHAR(255),
+                enlaces VARCHAR(255),
+                email VARCHAR(255),
+                createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+                updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                deletedAt DATETIME NULL
+            );
+        `);
 
         await pool.query(`
         CREATE TABLE IF NOT EXISTS Generos_musicales(
@@ -95,76 +95,78 @@ deletedAt DATETIME NULL
 
         await pool.query(`
             CREATE TABLE IF NOT EXISTS Grupo_media(
-id INT AUTO_INCREMENT PRIMARY KEY,
-grupo_id INT,
-url VARCHAR(255) NOT NULL,
-mimetype VARCHAR(255),
-createdAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
-     `);
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                grupo_id INT,
+                url VARCHAR(255) NOT NULL,
+                mimetype VARCHAR(255),
+                createdAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+            );
+        `);
 
         await pool.query(`
-                CREATE TABLE IF NOT EXISTS Reservas(
-id INT AUTO_INCREMENT PRIMARY KEY,
-nombre VARCHAR(100) NOT NULL,
-sala_id INT,
-grupo_id INT,
-confirmada BOOLEAN DEFAULT false,
-FOREIGN KEY(sala_id) REFERENCES Salas(id),
-FOREIGN KEY(grupo_id) REFERENCES Grupos(id),
-createdAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-fecha VARCHAR(15),
-hora VARCHAR(15)
-);
-    `);
+            CREATE TABLE IF NOT EXISTS Reservas(
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                nombre VARCHAR(100) NOT NULL,
+                sala_id INT,
+                grupo_id INT,
+                confirmada BOOLEAN DEFAULT false,
+                FOREIGN KEY(sala_id) REFERENCES Salas(id),
+                FOREIGN KEY(grupo_id) REFERENCES Grupos(id),
+                createdAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                fecha VARCHAR(15),
+                hora VARCHAR(15)
+            );
+        `);
 
         await pool.query(`
-                CREATE TABLE IF NOT EXISTS Sala_comments(
-id INT AUTO_INCREMENT PRIMARY KEY,
-descripcion TEXT,
-createdAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-sala_id INT,
-FOREIGN KEY(sala_id) REFERENCES Salas(id)
-);
-    `);
+            CREATE TABLE IF NOT EXISTS Sala_comments(
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                descripcion TEXT,
+                createdAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                sala_id INT,
+                grupo_id INT,
+                FOREIGN KEY(sala_id) REFERENCES Salas(id),
+                FOREIGN KEY (grupo_id) REFERENCES Grupos(id)
+            );
+        `);
 
         await pool.query(`
             CREATE TABLE IF NOT EXISTS Grupo_comments(
-id INT AUTO_INCREMENT PRIMARY KEY,
-descripcion TEXT,
-createdAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-grupo_id INT,
-FOREIGN KEY(grupo_id) REFERENCES Grupos(id)
-);
-
-    `);
-
-        await pool.query(`
-                CREATE TABLE IF NOT EXISTS votos_grupos(
-id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-value TINYINT UNSIGNED NOT NULL,
-grupo_id INT,
-voto_sala_id INT,
-createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
-FOREIGN KEY (grupo_id) REFERENCES Grupos(id),
-FOREIGN KEY (voto_sala_id) REFERENCES Salas(id)
-);
-     `);
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                descripcion TEXT,
+                createdAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                grupo_id INT,
+                FOREIGN KEY(grupo_id) REFERENCES Grupos(id)
+            );
+        `);
 
         await pool.query(`
-                    CREATE TABLE IF NOT EXISTS votos_salas(
-id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-value TINYINT UNSIGNED NOT NULL,
-voto_grupo_id INT,
-sala_id INT,
-createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
-FOREIGN KEY (voto_grupo_id) REFERENCES Grupos(id),
-FOREIGN KEY (sala_id) REFERENCES Salas(id)
-);
-    `);
+            CREATE TABLE IF NOT EXISTS votos_grupos(
+                id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+                value TINYINT UNSIGNED NOT NULL,
+                grupo_id INT,
+                voto_sala_id INT,
+                createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (grupo_id) REFERENCES Grupos(id),
+                FOREIGN KEY (voto_sala_id) REFERENCES Salas(id)
+            );
+        `);
+
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS votos_salas(
+                id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+                value TINYINT UNSIGNED NOT NULL,
+                voto_grupo_id INT,
+                sala_id INT,
+                createdAt DATETIME DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (voto_grupo_id) REFERENCES Grupos(id),
+                FOREIGN KEY (sala_id) REFERENCES Salas(id)
+            );
+        `);
+
         console.log('¡Tablas creadas!');
     } catch (err) {
         console.error(err);
--- back/src/services/users/selectSalaByIdService.js
+++ back/src/services/users/selectSalaByIdService.js
@@ -6,28 +6,44 @@ const selectSalaByIdService = async (idSala) => {
         `
             SELECT 
                 S.id,
+                S.usuario_id,
                 S.nombre,
                 S.provincia,
+                S.genero,
                 S.direccion,
                 S.email,
                 S.precios,
                 S.capacidad,
                 S.descripcion,
                 S.equipamiento,
-                S.condiciones
-            FROM Salas S    
+                S.condiciones,
+                AVG(IFNULL(V.value, 0)) AS votes,
+                S.createdAt
+            FROM Salas S
+            LEFT JOIN votos_salas V ON V.sala_id = S.id           
+            INNER JOIN usuarios U ON U.id = S.usuario_id  
             WHERE S.id = ?
+            GROUP BY S.id
         `,
         [idSala]
     );
     // Obtenemos el array de fotos de la entrada.
-    // const [photos] = await pool.query(
-    //     `SELECT id, name FROM sala_media WHERE idSala = ?`,
-    //     [idSala]
-    // );
+    const [photos] = await pool.query(
+        `SELECT id, name FROM sala_fotos WHERE salaId = ?`,
+        [idSala]
+    );
 
     // // Agregamos el array de fotos a la entrada.
-    // entry[0].photos = photos;
+    entry[0].photos = photos;
+
+    // Obtenemos el array de los comentarios de la sala.
+    const [comments] = await pool.query(
+        `SELECT descripcion, grupo_id FROM sala_comments WHERE sala_id = ?`,
+        [idSala]
+    );
+
+    // // Agregamos el array de fotos a la entrada.
+    entry[0].comments = comments;
 
     return {
         entry,
