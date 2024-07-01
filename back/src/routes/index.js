a1dd492114bc3edbb62cdfa40dacfe0e3d8c79dd
Fran J Dorado <franciscojdorado@gmail.com>
2024-07-01 16:21:38 +0200

    Resuelto conflictos y añadido cambios no rastreados

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -4,11 +4,14 @@ import usersRouter from './usersRouter.js';
 import salasRouter from './salasRouter.js';
 import musicosRouter from './musicosRouter.js';
 import reservasRouter from './reservasRoutes.js';
+import salasSearchRouter from './salasSearchRouter.js';
+
 const router = express.Router();
 
 router.use(usersRouter);
 router.use(salasRouter);
 router.use(musicosRouter);
 router.use(reservasRouter);
+router.use('/search/salas', salasSearchRouter);
 
 export default router;
