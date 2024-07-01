609e427b5464c272df2a6151fbf5d2d66ff2d796
Isaquinsan12 <155665641+Isaquinsan12@users.noreply.github.com>
2024-07-01 19:02:09 +0200

    quitado validator express y desinstalado

--- back/src/routes/salasRouter.js
+++ back/src/routes/salasRouter.js
@@ -3,13 +3,12 @@ import { getSalaDetailController } from '../controllers/salas/index.js';
 import createSalaController from '../controllers/salas/createSalaController.js';
 import authUser from '../middleware/authUser.js';
 import salaExists from '../middleware/salaExists.js';
-import validatorSalas from '../middleware/validatorSalas.js';
 import uploadFiles from '../middleware/uploadFiles.js';
 
 const router = express.Router();
 
 //Endpoint crear nueva sala por usuario tipo sala
-router.post('/users/salas',authUser,salaExists,validatorSalas(),createSalaController);
+router.post('/users/salas',authUser,salaExists,createSalaController);
 
 // Endpoint detalle sala
 router.get('/salas/:idSala', getSalaDetailController);
