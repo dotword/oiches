9ee1ca496cdc4b8046214bace4327b435c1f99a8
Carmen Salgueiro <53618812+dotword@users.noreply.github.com>
2024-07-02 09:44:45 +0200

    Update index.js

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -4,7 +4,6 @@ import usersRouter from './usersRouter.js';
 import salasRouter from './salasRouter.js';
 import musicosRouter from './musicosRouter.js';
 import reservasRouter from './reservasRoutes.js';
-import salasSearchRouter from './salasSearchRouter.js';
 
 const router = express.Router();
 
@@ -12,6 +11,5 @@ router.use(usersRouter);
 router.use(salasRouter);
 router.use(musicosRouter);
 router.use(reservasRouter);
-router.use('/search/salas', salasSearchRouter);
 
 export default router;
