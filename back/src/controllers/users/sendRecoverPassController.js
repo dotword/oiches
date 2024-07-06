a98ff96f84ad0f0d9950ae78bce5e3a56ce5a8ec
Carmen Salgueiro <carmen@dotword.es>
2024-07-06 20:31:08 +0200

    new schema recoverpassword

--- back/src/controllers/users/sendRecoverPassController.js
+++ back/src/controllers/users/sendRecoverPassController.js
@@ -3,11 +3,16 @@ import randomstring from 'randomstring';
 import selectUserByEmail from '../../services/users/selectUserByEmailService.js';
 import updateRecoverPassService from '../../services/users/updateRecoverPassService.js';
 import generateErrorsUtil from '../../utils/generateErrorsUtil.js';
+import validateSchemaUtil from '../../utils/validateSchemaUtil.js';
+import sendRecoverPass from '../../schemas/users/sendRecoverPass.js';
 
 const sendRecoverPassController = async (req, res, next) => {
     try {
         //Obtengo el mail de la persona que quiere recuperar password
         const { email } = req.body;
+
+        // Validamos el body con Joi.
+        await validateSchemaUtil(sendRecoverPass, req.body);
 
         //Compruebo si existe usuario con el email proporcionado
         const user = await selectUserByEmail(email);
