b1a489871c56bac13662ce1eb375aa506c114d43
Carmen Salgueiro <carmen@dotword.es>
2024-07-08 13:01:32 +0200

    organizacion files

--- back/src/controllers/listas/listGenreController.js
+++ back/src/controllers/listas/listGenreController.js
@@ -1,17 +1,17 @@
+import listGenreService from '../../services/listas/listGenreService.js';
 
-import listGenreService from '../../services/musicos/listGenreService.js';
-
-const listGenreController = async(req,res,next)=>{
-try {
-    const genres = await listGenreService();
-    res.send({
-        status: 'ok',
-        data: {
-        genres
-        }})
-} catch (error) {
-    next(error)
-}
+const listGenreController = async (req, res, next) => {
+    try {
+        const genres = await listGenreService();
+        res.send({
+            status: 'ok',
+            data: {
+                genres,
+            },
+        });
+    } catch (error) {
+        next(error);
+    }
 };
 
 export default listGenreController;
