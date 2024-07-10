52c696b9b542ba26bdeb8127a1e74485aba88983
Angel Olivo <angelolivoandres1985@gmail.com>
2024-07-10 16:50:01 +0200

    comento para pull

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,11 +1,24 @@
 import './App.css';
+import LoginForm from './components/LoginForm';
+import { useAuth } from './authContext';
+import React from 'react';
 
 function App() {
-    return (
-        <>
-            <h1>Oiches</h1>
-        </>
-    );
+  const { auth, logout } = useAuth();
+
+  return (
+    <>
+      <h1>Oiches</h1>
+      {auth ? (
+        <div>
+          <h2>Bienvenido, {auth.userInfo.name}</h2>
+          <button onClick={logout}>Cerrar Sesión</button>
+        </div>
+      ) : (
+        <LoginForm />
+      )}
+    </>
+  );
 }
 
 export default App;
