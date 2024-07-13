6691b909f869c66f0de5d3a10f8f21300a748f87
Aitor Carreras <pqpiacarreras@gmail.com>
2024-07-13 19:48:00 +0800

    Register desktop

--- front/src/components/RegisterForm.jsx
+++ front/src/components/RegisterForm.jsx
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import { Input } from './Input.jsx';
 import { ToastContainer, toast, Bounce } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Link } from 'react-router-dom';
 
-export const RegisterForm = () => {
+export const RegisterForm = ({className}) => {
   const [error, setError] = useState(null);
 
   const handleSubmit = async (e) => {
@@ -53,7 +54,12 @@ export const RegisterForm = () => {
 
   return (
     <>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-5 p-2">
+    
+      <div className="absolute top-1 left-1/2 justify-between hidden w-1/2 p-5 lg:flex lg:text-black ">
+        <button className="flex gap-1"><svg className="self-center" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="m7.825 13l4.9 4.9q.3.3.288.7t-.313.7q-.3.275-.7.288t-.7-.288l-6.6-6.6q-.15-.15-.213-.325T4.426 12t.063-.375t.212-.325l6.6-6.6q.275-.275.688-.275t.712.275q.3.3.3.713t-.3.712L7.825 11H19q.425 0 .713.288T20 12t-.288.713T19 13z"/></svg> Back</button>
+        <p>¿Ya tienes una cuenta?<Link to='/login' className=" text-yellow-200"> Log in</Link></p>
+      </div>
+      <form onSubmit={handleSubmit} className={className}>
         <h3 className="text-4xl">Registro</h3>
         <p>Lorem ipsum dolor sit amet consectetur</p>
         <hr />
