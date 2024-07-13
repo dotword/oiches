8a6ebd7f0ac1fa9ed18d3596017c05eadd2aef3c
Isaquinsan12 <155665641+Isaquinsan12@users.noreply.github.com>
2024-07-13 18:30:11 +0200

    actualizado

--- back/src/services/salas/insertVoteSalaService.js
+++ back/src/services/salas/insertVoteSalaService.js
@@ -1,42 +1,52 @@
 import getPool from "../../database/getPool.js";
 import generateErrorsUtil from "../../utils/generateErrorsUtil.js";
 import { v4 as uuidv4 } from 'uuid';
-import getGrupoById from "../grupos/getGrupoIdService.js";
+import selectGrupoByIdService from '../grupos/selectGrupoByIdService.js';
 
-const insertVoteSalaService = async (value, idSala, voto_grupo_id) => {
+const insertVoteSalaService = async (value, idSala, idGrupo) => {
     const pool = await getPool();
 
-    // Verify the voto_grupo_id value
-    await getGrupoById(voto_grupo_id);
+    try {
+        // Verificar si el grupo existe
+        await selectGrupoByIdService(idGrupo);
 
-    const [votes] = await pool.query(
-        `
-            SELECT id FROM votos_salas
-            WHERE voto_grupo_id=? AND sala_id=?
-        `,
-        [voto_grupo_id, idSala]
-    );
+        const [votes] = await pool.query(
+            `
+                SELECT id FROM votos_salas
+                WHERE voto_grupo_id=? AND sala_id=?
+            `,
+            [idGrupo, idSala]
+        );
 
-    if (votes.length) throw generateErrorsUtil('No se puede votar más de una vez la entrada', 409);
+        if (votes.length) {
+            throw generateErrorsUtil('No se puede votar más de una vez la entrada', 409);
+        }
 
-    const id = uuidv4(); // Generate a unique UUID for the 'id' column
+        const id = uuidv4(); // Generar un UUID único para la columna 'id'
 
-    await pool.query(
-        `
-            INSERT INTO votos_salas (id, value, sala_id, voto_grupo_id)
-            VALUES (?, ?, ?, ?)
-        `,
-        [id, value, idSala, voto_grupo_id]
-    );
+        await pool.query(
+            `
+                INSERT INTO votos_salas (id, value, voto_grupo_id, sala_id)
+                VALUES (?, ?, ?, ?)
+            `,
+            [id, value, idGrupo, idSala]
+        );
 
-    const [avgVotes] = await pool.query(
-        `
-            SELECT AVG(value) AS avg FROM votos_salas WHERE sala_id=?
-        `,
-        [idSala]
-    );
+        const [avgVotes] = await pool.query(
+            `
+                SELECT AVG(value) AS avg FROM votos_salas WHERE sala_id=?
+            `,
+            [idSala]
+        );
 
-    return Number(avgVotes[0].avg);
+        return Number(avgVotes[0].avg);
+    } catch (error) {
+        if (error.httpStatus === 404) {
+            throw generateErrorsUtil('Grupo no encontrado', 404);
+        } else {
+            throw error;
+        }
+    }
 };
 
 export default insertVoteSalaService;
