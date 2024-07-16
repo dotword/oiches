e1f97d88b0b98c78f1d22358f179eafe6d795a65
Carmen Salgueiro <carmen@dotword.es>
2024-07-16 13:43:28 +0200

    new

---
+++ front/src/services/getDataUserLoggedService.jsx
@@ -0,0 +1,17 @@
+const getDataUserLoggedService = async ({ token }) => {
+    const url = `${import.meta.env.VITE_API_URL_BASE}/users`;
+    console.log(url);
+    const response = await fetch(url, {
+        headers: {
+            token: token,
+        },
+    });
+
+    const json = await response.json();
+
+    if (!response.ok) throw new Error(json.message);
+
+    return json.data.user;
+};
+
+export default getDataUserLoggedService;
