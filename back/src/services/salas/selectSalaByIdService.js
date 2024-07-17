4ba8aba9121c386b7ff0bc87148ba1cfd9dc32b7
Carmen Salgueiro <carmen@dotword.es>
2024-07-17 05:35:20 +0200

    Actualizados salas y grupos

--- back/src/services/salas/selectSalaByIdService.js
+++ back/src/services/salas/selectSalaByIdService.js
@@ -6,7 +6,6 @@ const selectSalaByIdService = async (idSala) => {
         `
             SELECT 
                 S.id,
-                S.usuario_id,
                 S.nombre,
                 (SELECT provincia FROM provincias WHERE provincias.id = S.provincia) AS provincia,
                 (SELECT nombre FROM generos_musicales WHERE generos_musicales.id = S.generos) AS genero,
@@ -18,9 +17,9 @@ const selectSalaByIdService = async (idSala) => {
                 S.condiciones,
                 S.horaReservasStart,
                 S.horaReservasEnd,
-                AVG(IFNULL(V.voto, 0)) AS votes,
-                (SELECT comentario FROM votos_salas WHERE votos_salas.salaVotada = S.id) AS comentario,
-                S.createdAt
+                (SELECT email FROM usuarios WHERE usuarios.id = S.usuario_id) AS email,
+                S.createdAt,
+                AVG(IFNULL(V.voto, 0)) AS votos
             FROM Salas S
             LEFT JOIN votos_salas V ON V.salaVotada = S.id           
             INNER JOIN usuarios U ON U.id = S.usuario_id
@@ -30,33 +29,51 @@ const selectSalaByIdService = async (idSala) => {
         [idSala]
     );
 
-    if (entry.length === 0) {
-        return null;
-    }
+    // Obtenemos el array de los comentarios de la sala
+    const [comentarios] = await pool.query(
+        `
+            SELECT
+                comentario,
+                voto,
+                (SELECT nombre FROM grupos WHERE grupos.id = votos_salas.grupoVota) AS grupoVota
+            FROM votos_salas 
+            JOIN grupos ON grupos.id = votos_salas.grupoVota
+            WHERE salaVotada = ?
+  
+        `,
+        [idSala]
+    );
+    // Agregamos el array de los media del grupo.
+    entry[0].comentarios = comentarios;
 
     // Fetch photos, comments, and reservations
     const [photos] = await pool.query(
         `SELECT id, name FROM sala_fotos WHERE salaId = ?`,
         [idSala]
     );
+
     const [reservations] = await pool.query(
         `
             SELECT
-                R.grupo_id,
+            R.grupo_id,
                 G.nombre AS grupo,
                 R.fecha,
                 R.horaInicio,
                 R.horaFin,
                 R.confirmada
-            FROM Reservas R
-            LEFT JOIN Grupos G ON G.id= R.grupo_id
+                FROM Reservas R
+                LEFT JOIN Grupos G ON G.id= R.grupo_id
             WHERE sala_id = ?
-        `,
+            `,
         [idSala]
     );
 
+    if (entry.length === 0) {
+        return null;
+    }
     return {
         ...entry[0],
+        comentarios,
         photos,
         reservations,
     };
