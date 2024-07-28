4dc206eec8e6935b7e76f49677ffc65e928bc3c2
Aitor Carreras <pqpiacarreras@gmail.com>
2024-07-28 16:45:50 +0200

    Home fix

--- back/src/services/grupos/listGruposService.js
+++ back/src/services/grupos/listGruposService.js
@@ -6,21 +6,33 @@ export async function listGruposService(filters, sort) {
     const pool = await getPool();
 
     let query = `
-        SELECT 
-            g.id, g.nombre, g.usuario_id,
-            p.provincia AS provincia_nombre,
-            gm.nombre AS genero_nombre,
-            gf.name AS primera_foto
-        FROM grupos g
-        JOIN provincias p ON g.provincia = p.id
-        JOIN generos_musicales gm ON g.generos = gm.id
-        LEFT JOIN (
-            SELECT grupoId, name, MIN(createdAt) AS first_photo_date
-            FROM grupo_fotos 
-            GROUP BY grupoId, name
-        ) gf ON g.id = gf.grupoId
-        WHERE 1=1
+    SELECT 
+        g.id, 
+        g.nombre, 
+        g.usuario_id,
+        p.provincia AS provincia_nombre,
+        gm.nombre AS genero_nombre,
+        JSON_ARRAYAGG(
+            CASE 
+                WHEN gf.name NOT LIKE '%.pdf' THEN JSON_OBJECT('name', gf.name, 'createdAt', gf.createdAt) 
+                ELSE NULL 
+            END
+        ) AS fotos,
+        GROUP_CONCAT(
+            CASE 
+                WHEN gf.name LIKE '%.pdf' THEN gf.name 
+            END 
+            ORDER BY gf.createdAt
+        ) AS pdfs,
+        COALESCE(SUM(v.voto), 0) AS votos
+    FROM grupos g
+    JOIN provincias p ON g.provincia = p.id
+    JOIN generos_musicales gm ON g.generos = gm.id
+    LEFT JOIN grupo_fotos gf ON g.id = gf.grupoId
+    LEFT JOIN votos_grupos v ON g.id = v.voto
+    WHERE 1=1
     `;
+
     const queryParams = [];
 
     if (filters.nombre) {
@@ -38,8 +50,7 @@ export async function listGruposService(filters, sort) {
         queryParams.push(`%${filters.generos}%`);
     }
 
-    query +=
-        ' GROUP BY g.id, g.nombre, g.usuario_id, p.provincia, gm.nombre, gf.name';
+    query += ' GROUP BY g.id, g.nombre, g.usuario_id, p.provincia, gm.nombre';
 
     if (sort && sort.field && sort.order) {
         query += ` ORDER BY ${sort.field} ${sort.order}`;
--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -6,14 +6,18 @@ import SalaCard from './SalaCard.jsx';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
+import SalaFilter from '../components/SalaFilter.jsx';
+import FetchSalasService from '../services/FetchSalasService.js';
+import SalasImg from '../assets/SalasMusicales.jpeg';
+import GruposImg from '../assets/Musicos.jpeg';
 const Home = () => {
     const [salas, setSalas] = useState([]);
     const [grupos,setGrupos] = useState('')
     const listRef = useRef()
     const { VITE_API_URL_BASE } = import.meta.env;
-
-
+    const [filteredSalas, setFilteredSalas] = useState([]);
+    const [error, setError] = useState('');
+    const imageUrl = `${import.meta.env.VITE_API_URL_BASE}/uploads/`
     useEffect(() => {
         const fetchData = async () => {
             const data = await fetch(`${VITE_API_URL_BASE}/salas`);
@@ -29,7 +33,7 @@ const Home = () => {
         fetchData()
     },[])
     const settings = {
-        infinite: true,
+        infinite: false,
         speed: 500,
         slidesToShow: 4,
         slidesToScroll: 1,
@@ -39,8 +43,8 @@ const Home = () => {
             breakpoint: 1024,
             settings: {
               slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
+              slidesToScroll: 1,
+              infinite: false,
               dots: true
             }
           },
@@ -48,7 +52,7 @@ const Home = () => {
             breakpoint: 800,
             settings: {
               slidesToShow: 2,
-              slidesToScroll: 2,
+              slidesToScroll: 1,
               initialSlide: 2
             }
           },
@@ -61,7 +65,18 @@ const Home = () => {
           }
         ]
       };
+      
+      const handleFilterChange = async (filters) => {
+        try {
+            const filtered = await FetchSalasService(filters);
+            setSalas(filtered);
+        } catch (error) {
+            console.error('Error filtering salas:', error);
+            setError('Hubo un error al filtrar las salas');
+        }
+    };
 console.log(salas);
+console.log(grupos);
     return (
         <>
             <motion.div
@@ -69,8 +84,20 @@ console.log(salas);
                 animate={{ opacity: 1, height: '100%' }}
                 exit={{ opacity: 0, height: 0 }}
             >
-                <Header txt="Encuentra tu Banda Sonora" />
+                <Header />
+                <div className="hero w-full bg-gray-200 flex flex-col">
+                <h1 className="text-4xl font-bold">Encuentra tu Banda Sala</h1>
+                <p className="text-xl mt-4 text-center">
+                    Et has minim elitr intellegat. Mea aeterno eleifend antiopam
+                    ad, nam no suscipit quaerendum. At nam minimum ponderum. Est
+                    audiam animal molestiae te.
+                </p>
+            </div>
+            <div className="sala-filter-form-container">
+                <SalaFilter onFilterChange={handleFilterChange} />
+            </div>
                 <main className='flex flex-col gap-6 max-w-screen-lg mx-auto'>
+                    
                     <section className='grid gap-6'>
                         <div className='flex justify-between'>
                             <h2>Bandas/Musicos recomendados</h2>
@@ -98,17 +125,37 @@ console.log(salas);
                         <ul ref={listRef} className='max-w-xs md:max-w-3xl sm:max-w-md  lg:max-w-screen-lg mx-auto'>
                             <Slider className='rounded-xl' {...settings}>
                                 {grupos && grupos.map((grupo) => (
-
-                                    <SalaCard key={grupo.id} sala={grupo} />
-                                      
+                                    
+                                    <div key={grupo.id} className="sala-card bg-gray-800 text-white p-4 rounded-lg shadow-lg">
+                                        <img
+                                            src={`${VITE_API_URL_BASE}/uploads/${grupo.fotos[0].name}`}
+                                            alt={grupo.nombre}
+                                            className="sala-card-image w-full h-full object-cover rounded-lg"
+                                        />
+                                        <h2 className="sala-card-title text-xl font-bold mt-4">
+                                            {grupo.nombre}
+                                        </h2>
+                                        <p className="sala-card-genre text-gray-400">{grupo.genero_nombre}</p>
+                                        <p className="sala-card-province text-gray-400">{grupo.provincia_nombre}</p>
+                                        <p className="sala-card-votes text-gray-400">
+                                            Votos: {grupo.media_votos}
+                                        </p>
+                                    </div>
+                                
                                 ))}
                             </Slider>
                         </ul>
                         </div>
                     </section>
-                    <section className='flex justify-around'>
-                        <Link to={"/grupos"}><img src="" alt="" />Musicos/Bandas</Link>
-                        <Link to={"/salas"}><img src="" alt="" />Salas</Link>
+                    <section className='flex flex-col mx-auto my-10 gap-6 md:flex-row md:justify-around md:mx-0 md:mt-40 '>
+                        <Link className='relative hover:scale-110 transition-all' to={"/grupos"}>
+                            <img className='w-72 rounded-xl' src={GruposImg} alt=""/>
+                            <span className='absolute bottom-3 px-2 z-50 text-xl w-full text-white bg-black bg-opacity-65'>Musicos/Bandas</span>
+                        </Link>
+                        <Link className='relative hover:scale-110 transition-all' to={"/salas"}>
+                            <img className='w-72 h-full rounded-xl' src={SalasImg} alt=""/>
+                            <span className='absolute bottom-3 px-2 z-50 text-xl text-white w-full bg-black bg-opacity-65'>Salas Musicales</span>
+                        </Link>
                     </section>
                 </main>
             </motion.div>
