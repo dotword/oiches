61346e56995bdf4b87e62eb6a51bf626d9c7d8ff
Fran J Dorado <franciscojdorado@gmail.com>
2024-07-28 11:23:04 +0200

    Se añade star votacion y se genera css

--- front/src/components/SalaList.jsx
+++ front/src/components/SalaList.jsx
@@ -3,7 +3,7 @@ import SalaCard from './SalaCard';
 
 const SalaList = ({ salas }) => {
     return (
-        <div className="sala-list grid gap-8 p-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <div className="sala-list">
             {salas.map((sala) => (
                 <SalaCard key={sala.id} sala={sala} />
             ))}
