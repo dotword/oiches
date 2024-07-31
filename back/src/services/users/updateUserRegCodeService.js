c74809b6aae0e24ed4a436e90d68005da78ced8b
Carmen Salgueiro <carmen@dotword.es>
2024-07-31 16:27:22 +0200

    Editar email y password fix

--- back/src/services/users/updateUserRegCodeService.js
+++ back/src/services/users/updateUserRegCodeService.js
@@ -1,10 +1,5 @@
-
 // Importamos la función que devuelve una conexión con la base de datos.
 import getPool from '../../database/getPool.js';
-
-//importamos funcion para generar errores
-import generateErrorsUtil from '../../utils/generateErrorsUtil.js';
-
 
 // Función que realiza una consulta a la base de datos para activar un usuario.
 const updateUserRegCodeService = async (registrationCode) => {
@@ -16,7 +11,6 @@ const updateUserRegCodeService = async (registrationCode) => {
         [registrationCode]
     );
 
-    
     // if(!user.length) throw generateErrorsUtil('Codigo de registro incorrecto', 403);
 
     // Actualizamos el usuario.
