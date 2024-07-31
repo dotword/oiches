588e4a028bc07cb2ceb07285b1454a23000bab6b
Carmen Salgueiro <carmen@dotword.es>
2024-07-31 19:29:29 +0200

    Repeat password en Edit user y fixed menu

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -26,7 +26,7 @@ const Header = ({ txt }) => {
                     <div
                         className={
                             isNavOpen
-                                ? 'flex absolute w-full h-screen top-0 left-0 bg-white z-20 flex-col items-center justify-evenly'
+                                ? 'flex fixed w-full h-screen top-0 left-0 bg-white z-20 flex-col items-center justify-evenly'
                                 : 'hidden'
                         }
                     >
