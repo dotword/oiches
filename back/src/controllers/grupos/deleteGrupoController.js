54f84ca1d92ecda5f05b7ad5b6cb34aa3bf5d553
Aitor Carreras <pqpiacarreras@gmail.com>
2024-08-06 11:42:56 +0200

    Fix delete group

--- back/src/controllers/grupos/deleteGrupoController.js
+++ back/src/controllers/grupos/deleteGrupoController.js
@@ -2,8 +2,8 @@ import { deleteGrupoService } from '../../services/grupos/deleteGrupoService.js'
 
 export const deleteGrupoController = async (req, res, next) => {
     try {
-        const { grupoId } = req.params;
-        await deleteGrupoService(grupoId);
+        const { idGrupo } = req.params;
+        await deleteGrupoService(idGrupo);
         res.status(200).json({
             message: 'El grupo se ha eliminado con exito.',
         });
--- back/src/services/grupos/deleteGrupoService.js
+++ back/src/services/grupos/deleteGrupoService.js
@@ -1,47 +1,67 @@
 import getPool from '../../database/getPool.js';
 
 export const deleteGrupoService = async (grupoId) => {
+    console.log(grupoId);
+    const pool = await getPool();
+    const connection = await pool.getConnection();
+
     try {
-        const pool = await getPool();
+        await connection.beginTransaction();
+
         // Seleccionar grupo asociado al usuario
-        console.log(grupoId);
 
         // Eliminar media del grupo
-        await pool.query(`DELETE FROM grupo_media WHERE grupo_id = ?`, [
-            grupoId,
-        ]);
+         await connection.query(
+            `DELETE FROM grupo_media WHERE grupo_id = ?`,
+            [grupoId]
+        );
 
         // Eliminar generos de un grupo
-        await pool.query(`DELETE FROM generos_grupos WHERE grupoId = ?`, [
-            grupoId,
-        ]);
+         await connection.query(
+            `DELETE FROM generos_grupos WHERE grupoId = ?`,
+            [grupoId]
+        );
 
-        await pool.query(`DELETE FROM grupo_fotos WHERE grupoId = ?`, [
-            grupoId,
-        ]);
+        // Eliminar fotos del grupo
+       await connection.query(
+            `DELETE FROM grupo_fotos WHERE grupoId = ?`,
+            [grupoId]
+        );
 
         // Seleccionar reservas del grupo
-        const [reservas] = await pool.query(
+        const [reservas] = await connection.query(
             `SELECT id FROM reservas WHERE grupo_id = ?`,
             [grupoId]
         );
 
         for (const reserva of reservas) {
             // Eliminar votos y reservas asociadas
-            await pool.query(`DELETE FROM votos_grupos WHERE reservaId = ?`, [
-                reserva.id,
-            ]);
-            await pool.query(`DELETE FROM reservas WHERE id = ?`, [reserva.id]);
+           await connection.query(
+                `DELETE FROM votos_grupos WHERE reservaId = ?`,
+                [reserva.id]
+            );
+
+           await connection.query(
+                `DELETE FROM reservas WHERE id = ?`,
+                [reserva.id]
+            );
         }
 
         // Eliminar grupo
-        await pool.query(`DELETE FROM grupos WHERE id = ?`, [grupoId]);
+         await connection.query(
+            `DELETE FROM grupos WHERE id = ?`,
+            [grupoId]
+        );
+
+        await connection.commit();
 
         return {
             message: 'Grupo y todas sus relaciones eliminadas exitosamente',
         };
     } catch (error) {
-        console.log(error);
+        await connection.rollback();
         throw error;
+    } finally {
+        connection.release();
     }
 };
