eabba6943c78680e74d39443e32aabf26021c345
Fran J Dorado <franciscojdorado@gmail.com>
2024-10-20 19:46:58 +0200

    Actualizacion Vite a 5.4.8, eliminacion libreria lucide implementacion react icon, Seo mejora, Banner con CTA hero, Banner cta inferior, beneficios salas y grupos y proceso de alta Oiches

--- front/src/components/FeatureGridMusicos.jsx
+++ front/src/components/FeatureGridMusicos.jsx
@@ -10,51 +10,53 @@ const FeatureGridMusicos = () => {
             {/* Grid con dos columnas y distribución solicitada */}
             <section className="flex flex-col md:grid md:grid-cols-2 lg:grid-cols-4 mx-auto gap-4 md:justify-around md:mx-auto xl:w-1200">
                 {/* Tarjeta 1 */}
-                <div className="bg-white p-8 rounded-lg shadow-md border border-transparent hover:border-purple-600 transition-all">
-                    <div className="flex items-center justify-center w-12 h-12 bg-gradient-to-br from-purple-500 to-pink-500 rounded-full mb-2">
+                <div className="bg-white p-8 rounded-lg shadow-md border border-transparent hover:border-purple-600 transition-all text-center md:text-left">
+                    <div className="flex items-center justify-center w-12 h-12 bg-gradient-to-br from-purple-500 to-pink-500 rounded-full mb-2 mx-auto md:mx-0">
                         <FiMapPin className="w-6 h-6 text-white" />
                     </div>
-                    <h3 className="text-lg font-semibold">
+                    <h3 className="text-lg font-semibold text-center md:text-left">
                         Acceso directo a salas:
                     </h3>
-                    <p className="text-gray-600 mt-2">
+                    <p className="text-gray-600 mt-2 text-center md:text-left">
                         Encuentra y reserva las salas donde quieres tocar,
                         conoce su historia, equipamiento, etc.
                     </p>
                 </div>
                 {/* Tarjeta 2 */}
-                <div className="bg-white p-8 rounded-lg shadow-md border border-transparent hover:border-purple-600 transition-all">
-                    <div className="flex items-center justify-center w-12 h-12 bg-gradient-to-br from-purple-500 to-pink-500 rounded-full mb-2">
+                <div className="bg-white p-8 rounded-lg shadow-md border border-transparent hover:border-purple-600 transition-all text-center md:text-left">
+                    <div className="flex items-center justify-center w-12 h-12 bg-gradient-to-br from-purple-500 to-pink-500 rounded-full mb-2 mx-auto md:mx-0">
                         <FiCalendar className="w-6 h-6 text-white" />
                     </div>
-                    <h3 className="text-lg font-semibold">
+                    <h3 className="text-lg font-semibold text-center md:text-left">
                         Reservas sin complicaciones:
                     </h3>
-                    <p className="text-gray-600 mt-2">
+                    <p className="text-gray-600 mt-2 text-center md:text-left">
                         Puedes enviar solicitudes, gestionar fechas y estar en
                         contacto directo con las salas.
                     </p>
                 </div>
                 {/* Tarjeta 3 */}
-                <div className="bg-white p-8 rounded-lg shadow-md border border-transparent hover:border-purple-600 transition-all">
-                    <div className="flex items-center justify-center w-12 h-12 bg-gradient-to-br from-purple-500 to-pink-500 rounded-full mb-2">
+                <div className="bg-white p-8 rounded-lg shadow-md border border-transparent hover:border-purple-600 transition-all text-center md:text-left">
+                    <div className="flex items-center justify-center w-12 h-12 bg-gradient-to-br from-purple-500 to-pink-500 rounded-full mb-2 mx-auto md:mx-0">
                         <FiMusic className="w-6 h-6 text-white" />
                     </div>
-                    <h3 className="text-lg font-semibold">
+                    <h3 className="text-lg font-semibold text-center md:text-left">
                         Más oportunidades:
                     </h3>
-                    <p className="text-gray-600 mt-2">
+                    <p className="text-gray-600 mt-2 text-center md:text-left">
                         Descubre nuevas salas y toca en lugares que no sabías
                         que existían.
                     </p>
                 </div>
                 {/* Tarjeta 4 */}
-                <div className="bg-white p-8 rounded-lg shadow-md border border-transparent hover:border-purple-600 transition-all">
-                    <div className="flex items-center justify-center w-12 h-12 bg-gradient-to-br from-purple-500 to-pink-500 rounded-full mb-2">
+                <div className="bg-white p-8 rounded-lg shadow-md border border-transparent hover:border-purple-600 transition-all text-center md:text-left">
+                    <div className="flex items-center justify-center w-12 h-12 bg-gradient-to-br from-purple-500 to-pink-500 rounded-full mb-2 mx-auto md:mx-0">
                         <FiStar className="w-6 h-6 text-white" />
                     </div>
-                    <h3 className="text-lg font-semibold">Valoraciones:</h3>
-                    <p className="text-gray-600 mt-2">
+                    <h3 className="text-lg font-semibold text-center md:text-left">
+                        Valoraciones:
+                    </h3>
+                    <p className="text-gray-600 mt-2 text-center md:text-left">
                         Después de cada concierto, las salas podrán valorar la
                         experiencia, y tú también puedes dejar tus opiniones.
                         ¡Perfecto para crear conexiones!
--- front/src/components/FeatureGridSalas.jsx
+++ front/src/components/FeatureGridSalas.jsx
@@ -12,38 +12,42 @@ const FeatureGridSalas = () => {
             {/* Grid con dos columnas y distribución solicitada */}
             <section className="flex flex-col md:grid md:grid-cols-2 lg:grid-cols-4 mx-auto gap-4 md:justify-around md:mx-auto xl:w-1200">
                 {/* Tarjeta 1 */}
-                <div className="bg-white p-8 rounded-lg shadow-md border border-transparent hover:border-purple-600 transition-all">
-                    <div className="flex items-center justify-center w-12 h-12 bg-gradient-to-br from-purple-500 to-pink-500 rounded-full mb-2">
+                <div className="bg-white p-8 rounded-lg shadow-md border border-transparent hover:border-purple-600 transition-all text-center md:text-left">
+                    <div className="flex items-center justify-center w-12 h-12 bg-gradient-to-br from-purple-500 to-pink-500 rounded-full mb-2 mx-auto md:mx-0">
                         <FiMapPin className="w-6 h-6 text-white" />
                     </div>
-                    <h3 className="text-lg font-semibold">Más visibilidad:</h3>
-                    <p className="text-gray-600 mt-2">
+                    <h3 className="text-lg font-semibold text-center md:text-left">
+                        Más visibilidad:
+                    </h3>
+                    <p className="text-gray-600 mt-2 text-center md:text-left">
                         Los grupos están activamente buscando salas donde tocar.
                         ¡Haz que encuentren la tuya!
                     </p>
                 </div>
 
                 {/* Tarjeta 2 */}
-                <div className="bg-white p-8 rounded-lg shadow-md border border-transparent hover:border-purple-600 transition-all">
-                    <div className="flex items-center justify-center w-12 h-12 bg-gradient-to-br from-purple-500 to-pink-500 rounded-full mb-2">
+                <div className="bg-white p-8 rounded-lg shadow-md border border-transparent hover:border-purple-600 transition-all text-center md:text-left">
+                    <div className="flex items-center justify-center w-12 h-12 bg-gradient-to-br from-purple-500 to-pink-500 rounded-full mb-2 mx-auto md:mx-0">
                         <FiCalendar className="w-6 h-6 text-white" />
                     </div>
-                    <h3 className="text-lg font-semibold">
+                    <h3 className="text-lg font-semibold text-center md:text-left">
                         Nuevos descubrimientos
                     </h3>
-                    <p className="text-gray-600 mt-2">
+                    <p className="text-gray-600 mt-2 text-center md:text-left">
                         Conecta con grupos que quizás aún no conoces, y ofrece
                         tu espacio a artistas emergentes.
                     </p>
                 </div>
 
                 {/* Tarjeta 3 */}
-                <div className="bg-white p-8 rounded-lg shadow-md border border-transparent hover:border-purple-600 transition-all">
-                    <div className="flex items-center justify-center w-12 h-12 bg-gradient-to-br from-purple-500 to-pink-500 rounded-full mb-2">
+                <div className="bg-white p-8 rounded-lg shadow-md border border-transparent hover:border-purple-600 transition-all text-center md:text-left">
+                    <div className="flex items-center justify-center w-12 h-12 bg-gradient-to-br from-purple-500 to-pink-500 rounded-full mb-2 mx-auto md:mx-0">
                         <FiMusic className="w-6 h-6 text-white" />
                     </div>
-                    <h3 className="text-lg font-semibold">Control total:</h3>
-                    <p className="text-gray-600 mt-2">
+                    <h3 className="text-lg font-semibold text-center md:text-left">
+                        Control total:
+                    </h3>
+                    <p className="text-gray-600 mt-2 text-center md:text-left">
                         Eliges qué bandas reservas y cuándo. Además, puedes
                         valorar y comentar tu experiencia con cada grupo después
                         del concierto.
@@ -51,12 +55,14 @@ const FeatureGridSalas = () => {
                 </div>
 
                 {/* Tarjeta 4 */}
-                <div className="bg-white p-8 rounded-lg shadow-md border border-transparent hover:border-purple-600 transition-all">
-                    <div className="flex items-center justify-center w-12 h-12 bg-gradient-to-br from-purple-500 to-pink-500 rounded-full mb-2">
+                <div className="bg-white p-8 rounded-lg shadow-md border border-transparent hover:border-purple-600 transition-all text-center md:text-left">
+                    <div className="flex items-center justify-center w-12 h-12 bg-gradient-to-br from-purple-500 to-pink-500 rounded-full mb-2 mx-auto md:mx-0">
                         <FiStar className="w-6 h-6 text-white" />
                     </div>
-                    <h3 className="text-lg font-semibold">Crecimiento</h3>
-                    <p className="text-gray-600 mt-2">
+                    <h3 className="text-lg font-semibold text-center md:text-left">
+                        Crecimiento
+                    </h3>
+                    <p className="text-gray-600 mt-2 text-center md:text-left">
                         Más eventos, más música y más oportunidades para hacer
                         crecer tu sala.
                     </p>
