428ab99e3068cbb50a563d868a316666b5239a39
Fran J Dorado <franciscojdorado@gmail.com>
2024-12-04 03:58:31 +0100

    Actualización de la rama menuCss

--- front/src/components/HeaderHero.jsx
+++ front/src/components/HeaderHero.jsx
@@ -1,3 +1,73 @@
+// import { useState } from 'react';
+// import { RxHamburgerMenu } from 'react-icons/rx';
+// import { IoClose } from 'react-icons/io5';
+// import { Link } from 'react-router-dom';
+// import logoBlack from '../assets/Horizontal_negro.webp';
+// import Menu from './Menu';
+
+// const HeaderHero = () => {
+//     const [isNavOpen, setIsNavOpen] = useState(false);
+
+//     return (
+//         <header className="flex flex-col items-center justify-between pt-7 pb-4 px-6 md:max-w-7xl md:mx-auto">
+//             <nav className="hidden md:flex justify-between items-center w-full px-10 py-4 bg-white">
+//                 <Link to="/">
+//                     <img src={logoBlack} alt="logo" className="h-10" />
+//                 </Link>
+//                 <div className="flex space-x-6">
+//                     <Menu isMobile={false} />
+//                 </div>
+//             </nav>
+
+//             {/* Menú móvil */}
+//             <div className="md:hidden flex justify-between items-center w-full px-6">
+//                 <Link to="/">
+//                     <img src={logoBlack} alt="logo" className="h-10" />
+//                 </Link>
+//                 <RxHamburgerMenu
+//                     className="text-3xl cursor-pointer"
+//                     onClick={() => setIsNavOpen(true)}
+//                 />
+//             </div>
+
+//             {/* Fondo oscurecido y menú móvil */}
+//             {isNavOpen && (
+//                 <div
+//                     className="fixed inset-0 bg-black bg-opacity-50 z-20"
+//                     onClick={() => setIsNavOpen(false)}
+//                 ></div>
+//             )}
+//             <div
+//                 className={`fixed top-0 right-0 w-[85%] h-full bg-white z-30 transform transition-transform duration-500 ${
+//                     isNavOpen ? 'translate-x-0' : 'translate-x-full'
+//                 }`}
+//             >
+//                 {/* Logo y botón de cierre */}
+//                 <div className="flex flex-col items-center p-6">
+//                     <Link to="/">
+//                         <img
+//                             src={logoBlack}
+//                             alt="logo"
+//                             className="h-10  mt-12"
+//                         />
+//                     </Link>
+//                     <IoClose
+//                         className="absolute top-4 right-4 text-3xl cursor-pointer"
+//                         onClick={() => setIsNavOpen(false)}
+//                     />
+//                 </div>
+
+//                 {/* Menú */}
+//                 <div className="pt-0 px-6">
+//                     <Menu isMobile={true} />
+//                 </div>
+//             </div>
+//         </header>
+//     );
+// };
+
+// export default HeaderHero;
+
 import { useState } from 'react';
 import { RxHamburgerMenu } from 'react-icons/rx';
 import { IoClose } from 'react-icons/io5';
@@ -10,8 +80,9 @@ const HeaderHero = () => {
 
     return (
         <header className="flex flex-col items-center justify-between pt-7 pb-4 px-6 md:max-w-7xl md:mx-auto">
-            <nav className="hidden md:flex justify-between items-center w-full px-10 py-4 bg-white">
-                <Link to="/">
+            {/* Versión escritorio */}
+            <nav className="hidden md:flex justify-between items-center w-full px-10 py-4 bg-white rounded-lg">
+                <Link to="/" className="flex items-center">
                     <img src={logoBlack} alt="logo" className="h-10" />
                 </Link>
                 <div className="flex space-x-6">
@@ -21,7 +92,7 @@ const HeaderHero = () => {
 
             {/* Menú móvil */}
             <div className="md:hidden flex justify-between items-center w-full px-6">
-                <Link to="/">
+                <Link to="/" className="flex items-center">
                     <img src={logoBlack} alt="logo" className="h-10" />
                 </Link>
                 <RxHamburgerMenu
@@ -43,13 +114,9 @@ const HeaderHero = () => {
                 }`}
             >
                 {/* Logo y botón de cierre */}
-                <div className="flex flex-col items-center p-6">
+                <div className="flex flex-col items-center p-6 mt-8">
                     <Link to="/">
-                        <img
-                            src={logoBlack}
-                            alt="logo"
-                            className="h-10  mt-12"
-                        />
+                        <img src={logoBlack} alt="logo" className="h-10" />
                     </Link>
                     <IoClose
                         className="absolute top-4 right-4 text-3xl cursor-pointer"
@@ -57,8 +124,8 @@ const HeaderHero = () => {
                     />
                 </div>
 
-                {/* Menú */}
-                <div className="pt-0 px-6">
+                {/* Menú en móvil */}
+                <div className="pt-6 px-6">
                     <Menu isMobile={true} />
                 </div>
             </div>
--- front/src/components/Menu.jsx
+++ front/src/components/Menu.jsx
@@ -17,15 +17,24 @@ const Menu = ({ isMobile }) => {
     const backgroundClass = isMobile
         ? 'absolute inset-0 bg-purpleOiches scale-x-0 origin-right transition-transform duration-500 ease-in-out group-hover:scale-x-100'
         : '';
+
+    // Clases base para los botones dinámicos
     const buttonClass =
-        'text-lg font-semibold py-3 px-6 w-full text-center rounded-lg transition-all duration-300';
-    const loginButtonClass = `${buttonClass} border border-purpleOiches text-purpleOiches hover:bg-purpleOiches hover:text-white`;
-    const registerButtonClass = `${buttonClass} text-white bg-purpleOiches hover:bg-purple-700`;
+        'text-lg font-semibold py-3 px-6 text-center rounded-lg transition-all duration-300';
+
+    const loginButtonClass = `${buttonClass} border border-purpleOiches text-purpleOiches hover:bg-purpleOiches hover:text-white ${
+        isMobile ? 'w-full' : 'md:px-4 md:py-2 md:text-sm'
+    }`;
+    const registerButtonClass = `${buttonClass} text-white bg-purpleOiches hover:bg-purple-700 ${
+        isMobile ? 'w-full' : 'md:px-4 md:py-2 md:text-sm'
+    }`;
 
     return (
         <div
             className={`${
-                isMobile ? 'flex flex-col space-y-6' : 'flex space-x-10'
+                isMobile
+                    ? 'flex flex-col space-y-6'
+                    : 'md:flex md:flex-row md:space-x-10 md:items-center'
             }`}
         >
             {/* Enlaces generales */}
@@ -46,8 +55,14 @@ const Menu = ({ isMobile }) => {
                 <span className={innerTextClass}>Contacto</span>
             </NavLink>
 
-            {/* Botones dinámicos Login y Registro*/}
-            <div className="flex flex-col space-y-4">
+            {/* Botones dinámicos Login y Registro */}
+            <div
+                className={`${
+                    isMobile
+                        ? 'flex flex-col space-y-4'
+                        : 'flex md:flex-row md:space-x-4 md:items-center'
+                }`}
+            >
                 {!userLogged ? (
                     <>
                         <NavLink to="/login" className={loginButtonClass}>
