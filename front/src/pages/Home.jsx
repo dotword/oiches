f48d8b5153d7f7e325dda0ca2b8130eb5fa9e252
Carmen Salgueiro <carmen@dotword.es>
2024-12-08 10:51:44 +0100

    fixMenu

--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -78,6 +78,7 @@ const Home = () => {
                 initial={{ opacity: 0, y: 20 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.3 }}
+                className="overflow-hidden"
             >
                 <HeaderHero />
 
