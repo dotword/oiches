eae8934b0f9863f5a913b36c63e9a7700e26f7a8
Carmen Salgueiro <carmen@dotword.es>
2025-04-03 18:13:29 +0200

    Fix agencias especialidades

--- back/src/services/users/getUserOwnerService.js
+++ back/src/services/users/getUserOwnerService.js
@@ -44,24 +44,29 @@ const getUserOwnerService = async (userId) => {
             `SELECT * FROM agencias WHERE usuario_id = ?`,
             [userId]
         );
-        const [agenciaEspecialidad] = await pool.query(
-            `
-                SELECT
-                    I.especialidad_id idEspecialidad,
-                    E.especialidad
-                FROM agencias_especialidades I
-                LEFT JOIN agencias_especialidad E ON E.id = I.especialidad_id
-                WHERE I.agencia_id = ?
-                GROUP BY I.especialidad_id
-            `,
-            [agencias[0].id]
-        );
 
-        const [grupos] = await pool.query(
-            `SELECT id, nombre, provincia, honorarios, biografia, usuario_id, published, createdAt FROM grupos WHERE usuario_id = ?`,
-            [userId]
-        );
-        return { agencias, agenciaEspecialidad, grupos };
+        if (agencias.length === 0) {
+            return { agencias };
+        } else {
+            const [agenciaEspecialidad] = await pool.query(
+                `
+                    SELECT
+                        I.especialidad_id idEspecialidad,
+                        E.especialidad
+                    FROM agencias_especialidades I
+                    LEFT JOIN agencias_especialidad E ON E.id = I.especialidad_id
+                    WHERE I.agencia_id = ?
+                    GROUP BY I.especialidad_id
+                `,
+                [agencias[0].id]
+            );
+
+            const [grupos] = await pool.query(
+                `SELECT id, nombre, provincia, honorarios, biografia, usuario_id, published, createdAt FROM grupos WHERE usuario_id = ?`,
+                [userId]
+            );
+            return { agencias, agenciaEspecialidad, grupos };
+        }
     }
 };
 
