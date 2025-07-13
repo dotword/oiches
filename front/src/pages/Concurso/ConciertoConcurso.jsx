bfaf5035b4a92414786cf04385ca957729d082e2
Carmen Salgueiro <carmen@dotword.es>
2025-07-13 09:01:00 +0200

    Banner Concert Home

--- front/src/pages/Concurso/ConciertoConcurso.jsx
+++ front/src/pages/Concurso/ConciertoConcurso.jsx
@@ -4,7 +4,6 @@ import Footer from '../../components/Footer.jsx';
 import { FaArrowRight } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import StepsImg from '../../assets/Steps.jpg';
-import bannerBg from '../../assets/banner-concierto-background.jpg';
 import selectedBg from '../../assets/ganadoresBG.png';
 import sobreBG from '../../assets/sobreConcursoBg.jpg';
 import logoOiches from '../../assets/Horizontal_blanco.webp';
@@ -27,12 +26,7 @@ const ConciertoConcurso = () => {
                 <HeaderHero />
 
                 <main className="w-full">
-                    <section
-                        className="kilogram py-12 px-6 bg-no-repeat bg-cover"
-                        style={{
-                            backgroundImage: `url(${bannerBg})`,
-                        }}
-                    >
+                    <section className="kilogram py-12 px-6 bg-hero-concierto bg-no-repeat bg-cover">
                         <div className="w-full flex justify-center">
                             <h1 className="flex flex-col">
                                 <span className="text-white text-6xl z-10 sm:text-7xl md:text-9xl">
@@ -148,10 +142,10 @@ const ConciertoConcurso = () => {
                             <h2 className="font-bold text-4xl z-10">
                                 Andrei La Ley
                             </h2>
-                            <div className="mb-4 z-10">
+                            <div className="self-end mt-6 z-10">
                                 <Link
                                     to="/grupo/edc649e0-fbb8-45d3-ab9e-0a747459a966"
-                                    className="concurso_semifinal-link mt-6"
+                                    className="concurso_semifinal-link"
                                 >
                                     Conoce más
                                     <FaArrowRight className="ml-2" />
@@ -161,86 +155,103 @@ const ConciertoConcurso = () => {
                     </section>
 
                     <section
-                        className="text-white px-5 py-12 bg-no-repeat bg-cover bg-center md:flex md:justify-end md:px-10 md:py-20"
+                        className="text-white py-12 bg-no-repeat bg-cover bg-center md:py-36"
                         style={{
                             backgroundImage: `url(${StepsImg})`,
                         }}
                     >
-                        <div className="md:w-1/2">
-                            <h3 className="font-bold text-4xl mb-8 text-center md:text-left">
-                                Concierto Oiches 2025
-                            </h3>
-                            <div className="text-xl mb-8">
-                                <p className="flex gap-2 items-center mb-4">
-                                    <CiCalendar /> 20 de septiembre
-                                </p>
-                                <p className="flex gap-2 items-center mb-4">
-                                    <PiMapPinLight />
-                                    La Conservera: C. Costa, 5, 27863 Viveiro,
-                                    Lugo
-                                </p>
-                                <p className="flex gap-2 items-center mb-4">
-                                    <MdOutlineDoorSliding /> Apertura: 20:00
-                                </p>
-                                <p className="flex gap-2 items-center mb-4">
-                                    <HiOutlineTicket /> Entrada: 10 €
-                                </p>
-                            </div>
-                            <div>
-                                <p className="font-semibold text-lg">
-                                    Puntos de venta de entradas anticipadas
-                                </p>
-                                <p className="flex flex-col lg:flex-row lg:gap-1">
-                                    <span className="font-semibold">
-                                        La Conservera:
-                                    </span>{' '}
-                                    C/ Costa, 5, 27863, Viveiro (Lugo)
-                                </p>
-                                <p className="flex flex-col lg:flex-row lg:gap-1">
-                                    <span className="font-semibold">
-                                        Marlem (Dekada):
-                                    </span>{' '}
-                                    Av. Granxas, 13, 27861 Viveiro (Lugo)
-                                </p>
-                                <p className="flex flex-col lg:flex-row lg:gap-1">
-                                    <span className="font-semibold">
-                                        Galipizza Covas:
-                                    </span>{' '}
-                                    C/ Suasbarras, 40, 27861 Viveiro (Lugo)
-                                </p>
-                                <p className="font-semibold text-lg mt-6">
-                                    Venta online
-                                </p>
-                                <p>
-                                    Si quieres reservar tu entrada escríbenos al
-                                    email{' '}
-                                    <a
-                                        href="mailto:hola@oiches.com"
-                                        className="underline"
-                                    >
-                                        hola@oiches.com
-                                    </a>
-                                    .
-                                </p>
-                                <p>Asunto: Entradas Concurso Oiches 2025.</p>
-                                <p>
-                                    Indica tu nombre, número de entradas y tu
-                                    DNI.
-                                </p>
-                            </div>
-                            <div className="w-full max-w-700 h-96 mt-12">
-                                <iframe
-                                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d857.9150523171938!2d-7.590334168591286!3d43.675816596876096!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd321b0053e2bbfd%3A0x585f41cc8d68e814!2sConservera!5e0!3m2!1ses!2ses!4v1752049201075!5m2!1ses!2ses"
-                                    className="w-full h-full border-0"
-                                    allowFullScreen
-                                    loading="lazy"
-                                    referrerPolicy="no-referrer-when-downgrade"
-                                />
+                        {/* Contenedor para centrar el contenido y añadir márgenes */}
+                        <div className="max-w-7xl mx-auto px-5 md:px-10">
+                            {/* Este div ahora contiene las dos columnas */}
+                            <div className="lg:flex lg:gap-8">
+                                {/* --- Columna 1: Información de texto --- */}
+                                <div className="lg:w-1/2">
+                                    <h3 className="font-bold text-4xl mb-8 text-center md:text-left">
+                                        Concierto Oiches 2025
+                                    </h3>
+                                    <div className="text-xl mb-8">
+                                        <p className="flex gap-2 items-center mb-4">
+                                            <CiCalendar /> 20 de septiembre
+                                        </p>
+                                        <p className="flex gap-2 items-center mb-4">
+                                            <PiMapPinLight />
+                                            La Conservera: C. Costa, 5, 27863
+                                            Viveiro, Lugo
+                                        </p>
+                                        <p className="flex gap-2 items-center mb-4">
+                                            <MdOutlineDoorSliding /> Apertura:
+                                            20:00
+                                        </p>
+                                        <p className="flex gap-2 items-center mb-4">
+                                            <HiOutlineTicket /> Entrada: 10 €
+                                        </p>
+                                    </div>
+                                    <div>
+                                        <p className="font-semibold text-lg">
+                                            Puntos de venta de entradas
+                                            anticipadas
+                                        </p>
+                                        <p className="flex flex-col lg:flex-row lg:gap-1">
+                                            <span className="font-semibold">
+                                                La Conservera:
+                                            </span>{' '}
+                                            C/ Costa, 5, 27863, Viveiro (Lugo)
+                                        </p>
+                                        <p className="flex flex-col lg:flex-row lg:gap-1">
+                                            <span className="font-semibold">
+                                                Marlem (Dekada):
+                                            </span>{' '}
+                                            Av. Granxas, 13, 27861 Viveiro
+                                            (Lugo)
+                                        </p>
+                                        <p className="flex flex-col lg:flex-row lg:gap-1">
+                                            <span className="font-semibold">
+                                                Galipizza Covas:
+                                            </span>{' '}
+                                            C/ Suasbarras, 40, 27861 Viveiro
+                                            (Lugo)
+                                        </p>
+                                        <p className="font-semibold text-lg mt-6">
+                                            Venta online
+                                        </p>
+                                        <p>
+                                            Si quieres reservar tu entrada
+                                            escríbenos al email{' '}
+                                            <a
+                                                href="mailto:hola@oiches.com"
+                                                className="underline"
+                                            >
+                                                hola@oiches.com
+                                            </a>
+                                            .
+                                        </p>
+                                        <p>
+                                            Asunto: Entradas Concurso Oiches
+                                            2025.
+                                        </p>
+                                        <p>
+                                            Indica tu nombre, número de entradas
+                                            y tu DNI.
+                                        </p>
+                                    </div>
+                                </div>
+
+                                {/* --- Columna 2: Mapa --- */}
+                                <div className="lg:w-1/2 w-full h-96 mt-12 lg:mt-0 lg:h-[30rem]">
+                                    <iframe
+                                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d857.9150523171938!2d-7.590334168591286!3d43.675816596876096!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd321b0053e2bbfd%3A0x585f41cc8d68e814!2sConservera!5e0!3m2!1ses!2ses!4v1752049201075!5m2!1ses!2ses"
+                                        className="w-full h-full border-0"
+                                        allowFullScreen
+                                        loading="lazy"
+                                        referrerPolicy="no-referrer-when-downgrade"
+                                    />
+                                </div>
                             </div>
                         </div>
                     </section>
+
                     <section
-                        className="pt-10 pb-32 px-6 bg-no-repeat bg-cover bg-bottom text-white md:px-20"
+                        className="pt-20 pb-32 px-6 bg-no-repeat bg-cover bg-bottom text-white md:px-20 md:pt-40 md:pb-60"
                         style={{
                             backgroundImage: `url(${sobreBG})`,
                         }}
@@ -250,7 +261,7 @@ const ConciertoConcurso = () => {
                         </h3>
                         <div className="md:flex md:gap-8 md:justify-evenly md:mb-10">
                             <div>
-                                <div className="text-2xl mb-6 md:text-4xl">
+                                <div className="text-2xl mb-10 md:mb-12 md:text-4xl space-y-4 md:space-y-6">
                                     <p>Más de 135 artistas.</p>
                                     <p>3 proyectos musicales seleccionados.</p>
                                     <p>
@@ -258,7 +269,7 @@ const ConciertoConcurso = () => {
                                         gracias.
                                     </p>
                                 </div>
-                                <div className="md:text-xl">
+                                <div className="md:text-xl space-y-4 md:space-y-6">
                                     <p>Esto no ha hecho más que empezar. </p>
                                     <p>
                                         Gracias por votar, compartir y vivir
